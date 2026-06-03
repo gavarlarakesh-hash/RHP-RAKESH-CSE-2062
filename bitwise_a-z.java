d45788ae093664e1e13a8950f337878337562010
Gavarla Rakesh <gavarlarakesh@gmail.com>
2026-06-03 19:26:25 +0530

    Create bitwise_a-z.java

---
+++ bitwise_a-z.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class bitwise_a_z {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        int lowerMask = 0;
+        int upperMask = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if(ch >= 'a' && ch <= 'z') {
+                lowerMask |= (1 << (ch - 'a'));
+            }
+
+            else if(ch >= 'A' && ch <= 'Z') {
+                upperMask |= (1 << (ch - 'A'));
+            }
+        }
+
+        int allBits = (1 << 26) - 1;
+
+        if(lowerMask == allBits && upperMask == allBits)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
